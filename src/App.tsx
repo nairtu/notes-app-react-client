60590666fdd838427fc24a914924825cc84f616a
nairtu <nairtu@gmail.com>
2018-05-07 00:07:49 -0400

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import * as React from 'react';
+import {Navbar} from "react-bootstrap";
+import {Link} from "react-router-dom";
 import './App.css';
-
-import logo from './logo.svg';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div className="App container">
+          <Navbar fluid collapseOnSelect>
+              <Navbar.Header>
+                  <Navbar.Brand>
+                      <Link to="/">Scratch</Link>
+                  </Navbar.Brand>
+                  <Navbar.Toggle/>
+              </Navbar.Header>
+          </Navbar>
       </div>
     );
   }
